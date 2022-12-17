ece9ce1480caa1b88fe4aa9d69ca19a6e458a418
Sairahcaz <zacharias.creutznacher@gmail.com>
2022-12-17 15:34:35 +0100

    write function tests

---
+++ src/array_functions.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace LaracraftTech;
+
+use Illuminate\Support\Arr;
+
+/**
+* Get a key-value pair from the array, and remove it.
+*
+* @param  array  $array
+* @param  string|int  $key
+* @param  mixed  $default
+* @return mixed
+*/
+function array_pull_with_Key(&$array, $key, $default = null)
+{
+    //as func_get_args() loses references, we can't pass
+    //spreaded func_get_args() here...
+    $value = Arr::pull($array, $key, $default);
+
+    return [$key => $value];
+}
---
+++ tests/ArrayPullWithKeyTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use function LaracraftTech\array_pull_with_Key;
+
+beforeEach(function () {
+    $this->array = [
+        'a' => 1,
+        'b' => 2,
+        'c' => [
+            'd' => 3
+        ],
+    ];
+
+    $this->arrayOrg = $this->array;
+});
+
+it('pulls the value with its key', function () {
+    $pull = array_pull_with_Key($this->array, 'a');
+    expect($pull)->toEqual(['a' => 1]);
+});
+
+it('pulls the nested value with its key', function () {
+    $pull = array_pull_with_Key($this->array, 'c.d');
+    expect($pull)->toEqual(['c.d' => 3]);
+});
+
+it('removes the pulled key', function () {
+    array_pull_with_Key($this->array, 'a');
+
+    unset($this->arrayOrg['a']);
+    expect($this->array)->toEqual($this->arrayOrg);
+});
